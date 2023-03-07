8b409ace1cb3b50a7c71e63c13f0fcee7d4853d8
Владимир Солдатов <cbb110@rambler.ru>
2023-03-07 10:53:55 +0300

    Добавьте файлы проекта.

---
+++ NetServerClient/NetServerClient.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include<WS2tcpip.h>
+#pragma comment(lib,"ws2_32.lib")
+using namespace std;
+int main()
+{
+	//Инициализация winSock
+	WSADATA wsData;
+	WORD  ver = MAKEWORD(2,2);
+	int wsOK = WSAStartup(ver, &wsData);
+	if (wsOK != 0)
+	{
+		cerr << "";
+	}
+	return 0;
+}
+
