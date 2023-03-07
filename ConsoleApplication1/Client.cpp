20242bd9565f2ab89e71e8d91085fbdc159b92a8
Владимир Солдатов <cbb110@rambler.ru>
2023-03-07 17:06:28 +0300

    Fix

---
+++ ConsoleApplication1/Client.cpp
@@ -0,0 +1,76 @@
+﻿
+#include <iostream>
+#include <iostream>
+#include<WS2tcpip.h>
+#include"windows.h"
+#include <string>
+#pragma comment(lib,"ws2_32.lib")
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string ipAddress = "127.0.0.1";
+	int port = 54000;
+	//Инициализация winSock
+	WSADATA wsData; //https://learn.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-wsadata
+	WORD  ver = MAKEWORD(2, 2); //Собщим версию сокета https://learn.microsoft.com/en-us/windows/win32/winsock/initializing-winsock
+	int wsOK = WSAStartup(ver, &wsData);// Передаем структуру данных и версию сокета
+	if (wsOK != 0)
+	{
+		cerr << "Не получилось инициализиорвать WinSock!\n";
+		return 403;
+	}
+
+	//Создание сокета
+	SOCKET connection = socket(AF_INET, SOCK_STREAM, 0);  // https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-socket
+	if (connection == INVALID_SOCKET)
+	{
+		cerr << "Не могу создать сокет!\n";
+		return 404;
+	}
+
+	// Связать сокет спортом и IP - адресом
+	sockaddr_in  hint; //Сктрутура подсказок по сокету;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port); //Привязываем порт к сокету
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+	int connResult = connect(connection, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		cerr << "No connection " <<WSAGetLastError() <<"\n";
+		closesocket(connection);
+		WSACleanup();
+		return 0;
+	}
+	char buff[4096];
+	string userInput;
+	do
+	{
+		cout << "> ";
+		getline(cin, userInput);
+		if (userInput.size() > 0)
+		{
+			int sendResult = send(connection, userInput.c_str(), (int)userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR)
+			{
+				ZeroMemory(buff, 4096);
+				int bytesRecieved = recv(connection, buff, 4096, 0);
+				if (bytesRecieved > 0)
+				{
+					cout << "SERVER " << string(buff, 0, bytesRecieved);
+				}
+			}
+		}
+
+	} while (userInput.size() > 0);
+	int error = closesocket(connection);
+	// Выключаем WinSock
+	WSACleanup();
+	return 0;
+
+
+}
+
--- NetServerClient/NetServerClient.cpp
+++ NetServerClient/NetServerClient.cpp
@@ -1,17 +1,158 @@
 ﻿#include <iostream>
 #include<WS2tcpip.h>
+#include"windows.h"
 #pragma comment(lib,"ws2_32.lib")
 using namespace std;
 int main()
 {
+	setlocale(LC_ALL, "RUSSIAN");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	//Инициализация winSock
-	WSADATA wsData;
-	WORD  ver = MAKEWORD(2,2);
-	int wsOK = WSAStartup(ver, &wsData);
+	WSADATA wsData; //https://learn.microsoft.com/en-us/windows/win32/api/winsock/ns-winsock-wsadata
+	WORD  ver = MAKEWORD(2, 2); //Собщим версию сокета https://learn.microsoft.com/en-us/windows/win32/winsock/initializing-winsock
+	int wsOK = WSAStartup(ver, &wsData);// Передаем структуру данных и версию сокета
 	if (wsOK != 0)
 	{
-		cerr << "";
+		cerr << "Не получилось инициализиорвать WinSock!\n";
+		return 403;
 	}
-	return 0;
+
+	//Создание сокета
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);  // https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-socket
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Не могу создать сокет!\n";
+		return 404;
+	}
+
+	// Связать сокет спортом и IP - адресом
+	sockaddr_in  hint; //Сктрутура подсказок по сокету;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000); //Привязываем порт к сокету
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; //Можем также использовать другие порты
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	//Указать, что сокет включен для прослушивания порта
+	listen(listening, SOMAXCONN);
+
+	// Дождаться соединения
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	char host[NI_MAXHOST];// Client remont name
+	char service[NI_MAXHOST]; //Service, i.e. the client connect on
+	ZeroMemory(host, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXHOST);
+	if (getnameinfo((sockaddr *)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXHOST, 0) == 0)
+		//https://learn.microsoft.com/en-us/windows/win32/api/ws2tcpip/nf-ws2tcpip-getnameinfo
+	{
+		cout << host << " connection on port " << service << endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << " connection on port " << service << endl;
+	}
+
+
+	//Закрыть просулшивающий сокет
+	closesocket(listening);
+	char buff[4096];
+	char * text = new char[4096]{"\0"};
+	//В цикле ожижать поступление сообщений
+	
+	while (true)
+	{
+
+		ZeroMemory(buff, 4096);
+		//Ожидаем пока клиента получит ответ
+		int bytesRecieved;
+
+		bytesRecieved = recv(clientSocket, buff, 4096, 0);
+		if (bytesRecieved == SOCKET_ERROR)
+		{
+			cerr << "Ошибка получения в recv, выходим\n";
+			break;
+		}
+		if (bytesRecieved == 0)
+		{
+			cout << "Client отключился\n";
+			break;
+		}
+
+
+		//Возвращаем эхо сообщение на клиент
+
+		if (strcmp(buff, "\r\n") == 0)
+		{
+			if (strcmp(text, "exit") == 0)
+			{
+				send(clientSocket, "Good-Bye", (int)strlen("Good-Bye") + 1, 0);
+				delete[] text;
+				break;
+			}
+
+			send(clientSocket, text, (int)strlen(text) + 1, 0);
+			send(clientSocket, "\r\n", 3, 0);
+			ZeroMemory(text, 4096);
+		}
+		else
+		{
+			strcat_s(text, (int)(strlen(text) + strlen(buff)) + 1, buff);
+		}
+	
+	}
+
+		//Закрыть  сокет
+		int error = closesocket(clientSocket);
+		// Выключаем WinSock
+		WSACleanup();
+		return 0;
 }
 
+
+/*
+
+
+
+		ZeroMemory(buff, 4096);
+		//Ожидаем пока клиента получит ответ
+		int bytesRecieved;
+
+		bytesRecieved = recv(clientSocket, buff, 4096, 0);
+
+		if (bytesRecieved == SOCKET_ERROR)
+		{
+			cerr << "Ошибка получения в recv, выходим\n";
+			break;
+		}
+		if (bytesRecieved == 0)
+		{
+			cout << "Client отключился\n";
+			break;
+		}
+
+
+		//Возвращаем эхо сообщение на клиент
+
+		if (strcmp(buff, "\r\n") == 0)
+		{
+			if (strcmp(text, "exit") == 0)
+			{
+				send(clientSocket, "Good-Bye", (int)strlen("Good-Bye") + 1, 0);
+				delete[] text;
+				break;
+			}
+
+			send(clientSocket, text, (int)strlen(text) + 1, 0);
+			send(clientSocket, "\r\n", 3, 0);
+			ZeroMemory(text, 4096);
+		}
+		else
+		{
+				strcat_s(text, (int)(strlen(text) + strlen(buff)) + 1, buff);
+		}
+	}
+ */
